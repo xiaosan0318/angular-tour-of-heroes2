ac140be813d088a384960fd726060479b8266eb4
SanXiao <xiaosan@SanXiaodeMacBook-Pro.local>
2017-03-28 17:30:10 +0800

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} detail!</h2>
+      <div><label>id:</label><label>{{hero.id}}</label></div>
+      <div><label>name:</label><label>{{hero.name}}</label></div>
+      `
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'angular-tour-of heroes2!';
+  hero : Hero = {id: 22, name: 'xixi'};
 }
